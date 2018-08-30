e7c77a1301db056bc9b32b4a78bd342ebe13e346
Brittanie Pham <brittaniepham@gmail.com>
2018-08-29 20:54:00 -0600

    remove setters (found bug: total num of movies)

--- Movie.java
+++ Movie.java
@@ -22,24 +22,12 @@ public class Movie {
 		return title;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 	public String getGenre() {
 		return genre;
 	}
 
-	public void setGenre(String genre) {
-		this.genre = genre;
-	}
-
 	public int getYear() {
 		return year;
-	}
-
-	public void setYear(int year) {
-		this.year = year;
 	}
 
 	public int getId() {
